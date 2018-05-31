7cc3afd416bdaea220dc860abf7599485dc4727c
josmy <josemyjoseph@gmail.com>
2018-05-31 13:16:13 -0400

    First commit

---
+++ IdeaProjects/changeProgram/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double x, z, y;
+
+        x = 5;
+        y = 9.5;
+
+        z = x * y;
+
+        System.out.println("The product is " + z);
+
+    }
+}
